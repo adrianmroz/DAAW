1283dcaeaf8e4c267a160726aba7576f6f0fc055
Adam Kulczycki <kulczyckiadam@gmail.com>
2017-03-24 14:39:00 +0100

    nowy server, wyswietlanie wszystkich zakladek w bazie

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,52 @@
-var express     = require('express');
+const express	= require('express');
 var router      = express.Router();
-app = express();
+const path 		= require('path');
+const NodeCouchDb = require('node-couchdb');
+
+const couch = new NodeCouchDb({
+	auth: {
+        user: 'admin',
+        pass: '123456'
+    }
+});
+
+var nano   = require('nano')('http://localhost:5984')
+  , db     = nano.use('database')
+  ;
+
+var database = nano.db.use('database');
 
 
+
+router.get('/', function(req,res){
+	res.send('Working...');
+});
+
+
+router.post('/bookmarks', function(req, res){
+	couch.insert('database',{
+		id: req.body.id,
+		url: req.body.url
+	}).then(
+		function(data,headers,status){
+		res.send(status);
+	},
+	function(err){
+		res.send(err);
+	});
+});
+
+router.get('/bookmarks', function(req, res){
+	database.view('all_urls', 'all', function(err, body) {
+		var document=[];
+  if (!err) {
+    body.rows.forEach(function(doc) {
+      console.log(doc.value);
+			document.push(doc.value);
+    });
+		res.json(document);
+  }
+});
+});
 
 module.exports = router;  
