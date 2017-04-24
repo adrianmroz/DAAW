5671795464e85e1a29ff0a0b9cb6dfaa4ba678be
Adam Kulczycki <kulczyckiadam@gmail.com>
2017-04-24 13:52:31 +0200

    dodanie logowania i rejestracji do serwera

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,12 +9,112 @@ const couch = new NodeCouchDb({
         pass: '123456'
     }
 });
-
 var nano   = require('nano')('http://localhost:5984')
   , db     = nano.use('database')
   ;
-
 var database = nano.db.use('database');
+
+var expressJwt = require('express-jwt');
+var SECRET = 'mysecret';
+
+var nano2 = require('nano')('http://localhost:5984/users');
+var jwt = require('jsonwebtoken');
+//var _ = require('lodash');
+
+app = express();
+app.set('secret', SECRET);
+app.use('/api', expressJwt({
+            secret : SECRET
+        }))
+
+
+router.post('/authentication', function(req, res) {
+    //Client will call this method to query a token
+    var username = req.body.username;
+    var password = req.body.password;
+    nano2.get(username, null, function(err, body) {
+        if (err) {
+            res.status(401).send('Wrong user or password');
+            return;
+        } else {
+            console.log(body);
+            //properties contained in the token, of course you can add first name, last name and so on
+            var profile = {
+                username: username
+            };
+            // We are encoding the profile inside the token
+            var token = jwt.sign(profile, app.get('secret'), {
+                expiresIn: 60 * 5
+            });
+            res.json({
+                token: token
+            });
+        }
+    });
+});
+
+router.post('/users',function(req, res) {   ///trzeba zrobic validacje po froncie bo nazwa bazy musi byc z malej litery i nie moze zawierac dziwnych znakow
+//when signup method is called, we insert a new user in couchDB, username being the id of the record
+    nano2.insert({
+        'password': req.body.password
+    }, req.body.username, function(err, body, header) {
+        if (err) {
+            //if for example username already exists in database, an error will be thrown
+            console.log('user insertion error', err.message);
+            res.status(400).send(err.message);
+        }else{
+        console.log('you have inserted a new user: ' + req.body.username);
+        console.log(req.body);
+                   var profile = {
+                username: req.body.username
+            };
+            couch.createDatabase(req.body.username).then(() => {console.log("database "+req.body.username+" created")}, err => {
+                console.log("cant create database");
+            });
+            // We are encoding the profile inside the token
+            var token = jwt.sign(profile, app.get('secret'), {
+                expiresIn: 60 * 5
+            });
+            res.json({
+                token: token
+            });
+
+        }
+    });
+});
+
+
+router.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('secret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
 var api         = require('./routes/api');
 
+
 var port = process.env.PORT || 3000;
 
 
@@ -17,7 +18,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 
 app.use('/api', api);
 app.listen(port);
